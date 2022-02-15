52a11b8e5b6f9bd40edbd157377793917da451e0
Niam5 <ebakkedahl@gmail.com>
2022-02-14 23:14:28 -0600

    Add SPP Solocraft
    -Adds SoloCraft as an option to CMake to enable/disable mod
    -Updated CI files to test on push and build one daily release with SoloCraft
    
    Credit to acidmanifesto and the TrinityCore project for the heavy legwork on getting this created

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -1428,6 +1428,9 @@ class Unit : public WorldObject
         void SetMaxHealth(uint32 val);
         void SetHealthPercent(float percent);
         float ModifyHealth(float dVal);
+#ifdef BUILD_SOLOCRAFT
+        void SetFullHealth() { SetHealth(GetMaxHealth()); }
+#endif
         float OCTRegenHPPerSpirit() const;
         float OCTRegenMPPerSpirit() const;
 
