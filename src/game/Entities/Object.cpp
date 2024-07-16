c6eaf10f9cf200b24e5fb2be58302d301b02917f
Niam5 <ebakkedahl@gmail.com>
2024-07-15 20:43:09 -0700

    Fix elunaEvent init and de-init
    Fixes multiple crashes related to BG's
    Closes #1
    
    Co-Authored-By: Foe <Foereaper@users.noreply.github.com>

--- src/game/Entities/Object.cpp
+++ src/game/Entities/Object.cpp
@@ -1368,7 +1368,7 @@ bool WorldObject::HasStringId(uint32 stringId) const
 
 WorldObject::WorldObject() :
 #ifdef BUILD_ELUNA
-    elunaEvents(NULL),
+    elunaEvents(nullptr),
 #endif
     m_transport(nullptr), m_transportInfo(nullptr), m_isOnEventNotified(false),
     m_visibilityData(this), m_currMap(nullptr),
@@ -1381,7 +1381,7 @@ WorldObject::WorldObject() :
 WorldObject::~WorldObject()
 {
     delete elunaEvents;
-    elunaEvents = NULL;
+    elunaEvents = nullptr;
 }
 #endif
 
@@ -1391,20 +1391,12 @@ void WorldObject::CleanupsBeforeDelete()
     RemoveFromWorld();
 }
 
-#ifdef BUILD_ELUNA
-void WorldObject::Update(uint32 update_diff)
-{
-    elunaEvents->Update(update_diff);
-    m_heartBeatTimer.Update(update_diff);
-    while (m_heartBeatTimer.Passed())
-    {
-        m_heartBeatTimer.Reset(m_heartBeatTimer.GetExpiry() + GetHeartbeatDuration());
-        Heartbeat();
-    }
-}
-#else
 void WorldObject::Update(const uint32 diff)
 {
+#ifdef BUILD_ELUNA
+    if (elunaEvents)
+        elunaEvents->Update(diff);
+#endif
     m_heartBeatTimer.Update(diff);
     while (m_heartBeatTimer.Passed())
     {
@@ -1412,7 +1404,6 @@ void WorldObject::Update(const uint32 diff)
         Heartbeat();
     }
 }
-#endif
 
 void WorldObject::_Create(uint32 guidlow, HighGuid guidhigh, uint32 phaseMask)
 {
@@ -2194,21 +2185,6 @@ void WorldObject::SetMap(Map* map)
     // lets save current map's Id/instanceId
     m_mapId = map->GetId();
     m_InstanceId = map->GetInstanceId();
-#ifdef BUILD_ELUNA
-    //@todo: possibly look into cleanly clearing all pending events from previous map's event mgr.
-
-    // if multistate, delete elunaEvents and set to nullptr. events shouldn't move across states.
-    // in single state, the timed events should move across maps
-    if (!sElunaConfig->IsElunaCompatibilityMode())
-    {
-        delete elunaEvents;
-        elunaEvents = nullptr; // set to null in case map doesn't use eluna
-    }
-
-    if (Eluna* e = map->GetEluna())
-        if (!elunaEvents)
-            elunaEvents = new ElunaEventProcessor(e, this);
-#endif
 }
 
 void WorldObject::AddToWorld()
@@ -2221,6 +2197,12 @@ void WorldObject::AddToWorld()
             m_currMap->AddStringIdObject(stringId, this);
 
     Object::AddToWorld();
+
+#ifdef BUILD_ELUNA
+    if (Eluna* e = GetEluna())
+        if (!elunaEvents)
+            elunaEvents = new ElunaEventProcessor(e, this);
+#endif
 }
 
 void WorldObject::RemoveFromWorld()
@@ -2238,6 +2220,16 @@ void WorldObject::RemoveFromWorld()
             for (uint32 stringId : m_stringIds)
                 m_currMap->RemoveStringIdObject(stringId, this);
     }
+
+#ifdef BUILD_ELUNA
+    // if multistate, delete elunaEvents and set to nullptr. events shouldn't move across states.
+    // in single state, the timed events should move across maps
+    if (!sElunaConfig->IsElunaCompatibilityMode())
+    {
+        delete elunaEvents;
+        elunaEvents = nullptr; // set to null in case map doesn't use eluna
+    }
+#endif
 
     Object::RemoveFromWorld();
 }
