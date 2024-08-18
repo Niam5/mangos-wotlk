33ab1fe39c2347bd2d272bab344e30763ea0e56c
Niam5 <ebakkedahl@gmail.com>
2024-08-18 00:56:57 -0700

    Move Map OnCreate hook to better location

--- src/game/Maps/MapManager.cpp
+++ src/game/Maps/MapManager.cpp
@@ -31,6 +31,7 @@
 #include <future>
 
 #ifdef BUILD_ELUNA
+#include "LuaEngine/LuaEngine.h"
 #include "LuaEngine/ElunaConfig.h"
 #endif
 
@@ -159,6 +160,11 @@ Map* MapManager::CreateMap(uint32 id, const WorldObject* obj)
             m->Initialize();
         }
     }
+
+#ifdef BUILD_ELUNA
+    if (Eluna* e = m->GetEluna())
+        e->OnCreate(m);
+#endif
 
     return m;
 }
